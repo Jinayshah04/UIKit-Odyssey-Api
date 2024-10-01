f2d2a132bb6946e170093c43e4de7c08f29626a1
Jinayshah04 <shah0410jinay@gmail.com>
2024-10-01 12:58:22 +0530

    UikitOdysseyApi

---
+++ UIKit-Odyssey/Controllers/JokeModel.swift
@@ -0,0 +1,15 @@
+//
+//  JokeModel.swift
+//  UIKit-Odyssey
+//
+//  Created by mobile1 on 30/08/24.
+//
+
+import Foundation
+
+struct jokeTable{
+    let id:Int
+    let type:String
+    let setup:String
+    let punchline:String
+}
